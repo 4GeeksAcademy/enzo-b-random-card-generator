4d01fee99734887ce72c03aaf6bfd997104cf07c
enzobarrera <154301070+enzobarrera@users.noreply.github.com>
2025-02-04 00:58:59 +0000

    trabajo completado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
 import "bootstrap";
 import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let suits = [
+    "♦", 
+    "♥",
+    "♠",
+    "♣"
+  ]
+  let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "K", "Q","J"]
+  
+  let paloAleatorio = suits[Math.floor(Math.random() * suits.length)]
+  let value = values[Math.floor(Math.random() * values.length)]
+
+  let top = document.getElementById("topSuit")
+  let middle = document.getElementById("mainSuit")
+  let bottom = document.getElementById("bottomSuit") 
+
+  top.textContent = paloAleatorio
+  middle.textContent = value
+  bottom.textContent= paloAleatorio
+  
+  if (paloAleatorio == "♥" || paloAleatorio == "♦" ){
+    top.style.color ="red"
+    bottom.style.color = "red"
+  }
+  else{
+  }
+
+  
 };
